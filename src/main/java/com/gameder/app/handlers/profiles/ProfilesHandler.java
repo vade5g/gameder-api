cd49ccf85625df1442c8b106e278d09640e2d977
Aidar Mukhamadiev <noiresonn@gmail.com>
2017-05-07 11:12:56 +0300

    Make a dummy change

--- src/main/java/com/gameder/app/handlers/profiles/ProfilesHandler.java
+++ src/main/java/com/gameder/app/handlers/profiles/ProfilesHandler.java
@@ -9,9 +9,9 @@ public class ProfilesHandler {
     @RequestMapping(value = "/api/profiles", method = RequestMethod.GET)
     public ArrayList<Profile> getGamerList() {
         ArrayList<Profile> profiles = new ArrayList<Profile>();
-        profiles.add(new Profile("A Little Pony", "http://cartoonbros.com/wp-content/uploads/2016/04/My-Little-Pony-9.png"));
-        profiles.add(new Profile("Another Little Pony", "http://cartoonbros.com/wp-content/uploads/2016/04/My-Little-Pony-10.png"));
-        profiles.add(new Profile("Yet Another Little Pony", "http://cartoonbros.com/wp-content/uploads/2016/04/My-Little-Pony-11.png"));
+        profiles.add(new Profile("A Little Pony.", "http://cartoonbros.com/wp-content/uploads/2016/04/My-Little-Pony-9.png"));
+        profiles.add(new Profile("Another Little Pony.", "http://cartoonbros.com/wp-content/uploads/2016/04/My-Little-Pony-10.png"));
+        profiles.add(new Profile("Yet Another Little Pony.", "http://cartoonbros.com/wp-content/uploads/2016/04/My-Little-Pony-11.png"));
 
         return profiles;
     }
